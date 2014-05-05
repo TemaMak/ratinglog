17d31e55f4e7d5ab1513ad05017b7adad899291d
tema_mak <temamakdeveloper@gmail.com>
2014-05-05 15:33:05 +0400

    prepare to store

--- ratinglog/deinstall.sql
+++
@@ -1,5 +0,0 @@
---
--- SQL, которые надо выполнить движку при деактивации плагина админом. Вызывается на исполнение ВРУЧНУЮ в /plugins/PluginXxxxx.class.php в методе Deactivate()
--- Например:
-
-DROP TABLE IF EXISTS event_params;
